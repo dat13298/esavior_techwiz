c1a504cd41f28a41321110f0d27399a40b1fb0a3
thanh1149 <155382289+thanh1149@users.noreply.github.com>
2024-09-19 01:08:53 +0700

    add model

---
+++ lib/models/account.dart
@@ -0,0 +1,59 @@
+import 'feedbacks.dart';
+
+class Account{
+  final String fullName;
+  final String email;
+  final String phoneNumber;
+  final String passwordHash;
+  final String addressHome;
+  final String addressCompany;
+  final String role;
+  final List<Feedbacks> feedbacks;
+  final String status;
+
+  Account({
+    required this.fullName,
+    required this.phoneNumber,
+    required this.passwordHash,
+    required this.email,
+    required this.addressHome,
+    required this.addressCompany,
+    required this.feedbacks,
+    required this.role,
+    required this.status,
+  });
+
+  static Account fromMap(Map<String, dynamic> map) {
+    return Account(
+      fullName: map['fullName'] as String? ?? '',
+      email: map['email'] as String? ?? '',
+      phoneNumber: map['phoneNumber'] as String? ?? '',
+      passwordHash: map['passwordHash'] as String? ?? '',
+      addressHome: map['addressHome'] as String? ?? '',
+      addressCompany: map['addressCompany'] as String? ?? '',
+      role: map['role'] as String? ?? '',
+      status: map['status'] as String? ?? '',
+      feedbacks: (map['feedbacks'] as List<dynamic>?)
+          ?.map((feedbackMap) =>
+          Feedbacks.fromMap(feedbackMap as Map<String, dynamic>))
+          .toList() ??
+          [],
+    );
+  }
+  // Phương thức chuyển Album thành Map
+  Map<String, dynamic> toMap() {
+    return {
+      'email': email,
+      'fullName': fullName,
+      'phoneNumber': phoneNumber,
+      'passwordHash': passwordHash,
+      'addressHome': addressHome,
+      'addressCompany': addressCompany,
+      'role': role,
+      'feedbacks': feedbacks,
+      'status': status,
+    };
+  }
+
+
+}
