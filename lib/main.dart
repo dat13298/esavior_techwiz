e6662c74fbb4401a00a14a9868e02754fb7ab902
dat13298 <157354116+dat13298@users.noreply.github.com>
2024-09-19 09:24:10 +0700

    add import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:esavior_techwiz/connection/firebase_connection.dart';
-import 'package:esavior_techwiz/views/map_page.dart';
+import 'package:esavior_techwiz/views/map_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() async{
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
           useMaterial3: true
       ),
-      home: const MapPage(),
+      home: MapScreen(),
       // home: const WelcomePage(),
       debugShowCheckedModeBanner: false,
     );
