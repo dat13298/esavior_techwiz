d6425f202ca103e692cf270bc2332cd409c7382f
dat13298 <157354116+dat13298@users.noreply.github.com>
2024-09-19 20:50:50 +0700

    clear cmt noob

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:esavior_techwiz/views/welcome/welcome_page.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
+import 'controllers/map_controller.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -27,8 +29,27 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
         useMaterial3: true,
       ),
-      home: WelcomePage(),
+      home: HomeScreen(),
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showMapScreen(context); // Mở MapScreen khi nút bấm
+          },
+          child: Text('Show Map'),
+        ),
+      ),
     );
   }
 }
