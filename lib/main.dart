dc33f6c73fc341b3ee5ba6c9a654ccefe8884a6b
Khanh <kta87067@gmail.com>
2024-09-19 22:20:32 +0700

    add-about

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:esavior_techwiz/connection/firebase_connection.dart';
-import 'package:esavior_techwiz/views/about/about.dart';
-import 'package:esavior_techwiz/views/home/homepage.dart';
 import 'package:esavior_techwiz/views/map_screen.dart';
-import 'package:esavior_techwiz/views/profile/profile.dart';
 import 'package:esavior_techwiz/views/welcome/welcome_page.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+
+import 'controllers/map_controller.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +15,7 @@ void main() async {
 
   await FirestoreService.initializeFirebase();
 
-  runApp(AboutPage());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -30,8 +29,27 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
         useMaterial3: true,
       ),
-      home: WelcomePage(),
+      home: HomeScreen(),
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showMapScreen(context); // Mở MapScreen khi nút bấm
+          },
+          child: Text('Show Map'),
+        ),
+      ),
     );
   }
 }
