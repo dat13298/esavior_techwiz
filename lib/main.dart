1ef9e6d3c6242b6731c4cfd428a5914d0f02264a
Khanh <kta87067@gmail.com>
2024-09-19 19:50:35 +0700

    added-hompage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:esavior_techwiz/connection/firebase_connection.dart';
+import 'package:esavior_techwiz/views/home/homepage.dart';
 import 'package:esavior_techwiz/views/map_screen.dart';
+import 'package:esavior_techwiz/views/profile/profile.dart';
 import 'package:esavior_techwiz/views/welcome/welcome_page.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
