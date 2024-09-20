6c0672e35ba9a4eb32c3156256c5bb79359fc8c8
dat13298 <157354116+dat13298@users.noreply.github.com>
2024-09-20 13:47:35 +0700

    config data type and name

--- lib/models/booking.dart
+++ lib/models/booking.dart
@@ -3,45 +3,45 @@ import 'package:intl/intl.dart';
 
 class Booking {
   final String id;
-  final String carID;
-  final String userLongitude;
-  final String userLatitude;
-  final String locationLongitude;
-  final String locationLatitude;
-  final String userPhoneNumber;
+  final String? carID;
+  final double? startLongitude;
+  final double? startLatitude;
+  final double endLongitude;
+  final double endLatitude;
+  final String? userPhoneNumber;
   final Timestamp dateTime;
   final String type;
-  final String cost;
+  final double? cost;
   final String status;
-  final String driverPhoneNumber;
+  final String? driverPhoneNumber;
 
   Booking({
     required this.id,
-    required this.carID,
-    required this.userLongitude,
-    required this.userLatitude,
-    required this.locationLongitude,
-    required this.locationLatitude,
-    required this.userPhoneNumber,
+    this.carID,
+    this.startLongitude,
+    this.startLatitude,
+    required this.endLongitude,
+    required this.endLatitude,
+    this.userPhoneNumber,
     required this.dateTime,
     required this.type,
     required this.cost,
     required this.status,
-    required this.driverPhoneNumber,
+    this.driverPhoneNumber,
   });
 
   static Booking fromMap(Map<String,dynamic> map){
     return Booking(
       id: map['id'] as String,
       carID: map['carID'] as String,
-      userLongitude: map['userLongitude'] as String,
-      userLatitude: map['userLatitude'] as String,
-      locationLongitude: map['locationLongitude'] as String,
-      locationLatitude: map['locationLatitude'] as String,
+      startLongitude: map['startLongitude'] as double,
+      startLatitude: map['startLatitude'] as double,
+      endLongitude: map['endLongitude'] as double,
+      endLatitude: map['endLatitude'] as double,
       userPhoneNumber: map['userPhoneNumber'] as String,
       dateTime: map['dateTime'] as Timestamp,
       type: map['type'] as String,
-      cost: map['cost'] as String,
+      cost: map['cost'] as double,
       status: map['status'] as String,
       driverPhoneNumber: map['driverPhoneNumber'] as String,
     );
@@ -51,10 +51,10 @@ class Booking {
     return{
       'id':id,
       'carID':carID,
-      'userLongitude':userLongitude,
-      'userLatitude':userLatitude,
-      'locationLongitude':locationLongitude,
-      'locationLatitude':locationLatitude,
+      'userLongitude':startLongitude,
+      'userLatitude':startLatitude,
+      'locationLongitude':endLongitude,
+      'locationLatitude':endLongitude,
       'userPhoneNumber':userPhoneNumber,
       'dateTime':dateTime,
       'type':type,
