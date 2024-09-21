99e0735c04c2011ec5a428363d4a33af11ff9c0b
dat13298 <157354116+dat13298@users.noreply.github.com>
2024-09-21 13:54:28 +0700

    display map with address

--- lib/controllers/map_controller.dart
+++ lib/controllers/map_controller.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
+import '../services/address_service.dart';
 import '../services/open_route_service.dart';
 import '../views/map/map_screen.dart';
 
 //TODO: add account to this
-Future<void> showMapScreen(BuildContext context) async {
+Future<void> showMapScreen(BuildContext context, String? address) async {
   // Lấy vị trí hiện tại
+  String endLatitude;
+  String endLongitude;
+  LatLng end;
+
+  if(address!=null){
+    endLatitude = await getLatitudeFromAddress(address);
+    endLongitude = await getLongitudeFromAddress(address);
+    end = LatLng(double.parse(endLatitude), double.parse(endLongitude));
+  } else {
+    end = LatLng(21.035000, 105.825649);
+  }
   Position userPosition = await Geolocator.getCurrentPosition(
     desiredAccuracy: LocationAccuracy.bestForNavigation,
   );
   LatLng currentPosition = LatLng(userPosition.latitude, userPosition.longitude);
 
   // Điểm kết thúc
-  LatLng end = LatLng(21.035000, 105.825649);
+  // end = LatLng(21.035000, 105.825649);
 
   // Tải tuyến đường
   List<LatLng> routePoints = await getRouteCoordinates(currentPosition, end);
